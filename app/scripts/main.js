d50e031705a1275bf0060da63b3328aa5bae4421
wego <eguurt@gmail.com>
2015-09-02 14:19:31 +0300

    styled app as specs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
+var $ = function ( elem ) {
+    var _el = document.querySelectorAll( elem );
+    _el.toggleClass = function (selector){
+        for(var i = 0; i < _el.length; i+=1){
+            _el[i].classList.toggle(selector);
+        }
+    };
+    _el.on = function (event, callback){
+        for(var i = 0; i < _el.length; i+=1) {
+            _el[i].addEventListener(event, callback);
+        }
+    };
 
+    return _el;
+};
+
+
+$('.btn').on('click', function(){
+    $('.modal').toggleClass('toggle-hide');
+});
 console.log('ready!');
